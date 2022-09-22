caee13e9430e6e2deeaa3c0ddb4364f834e10f4d
Christopher Hosken <hoskenchristopher@gmail.com>
2022-09-22 18:36:58 +0700

    cleanup

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Christopher Hosken - Technical Artist
 
 
 //TODO:
- - Fix button animation (centering)
  - Contact form (need php?)
- - More works
  - About me
  - Footer
- - Stop title scroll
+ - Hide Scrolling
+ - Portfolio Page
+ - Better fonts
--- scripts/load.js
+++ scripts/load.js
@@ -1,12 +1,18 @@
 
 $(document).ready(function() {
+    if (sessionStorage.getItem("splash") !== "false") {
+        setTimeout(function() {
+            $("#splash").fadeOut(500);
+        }, 3000);
+    }
 
-
-    setTimeout(function() {
-        $("#university-container").fadeIn(350);
-        document.getElementById("university-container").style.display = "flex";
-        console.log("POPUP");
-    }, 500);
+    if (sessionStorage.getItem("popup") !== 'false') {
+        setTimeout(function() {
+            $("#university-container").fadeIn(350);
+            document.getElementById("university-container").style.display = "flex";
+        }, 500);
+        sessionStorage.setItem("popup", "false");
+    }
 });
 
 
