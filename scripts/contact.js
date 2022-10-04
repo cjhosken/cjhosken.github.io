4bfd840c97b36ec7e82fd9e3764488fb6dd06f0b
Christopher Hosken <hoskenchristopher@gmail.com>
2022-10-04 19:23:00 +0700

    code cleanup

---
+++ scripts/contact.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    document.getElementById("portfolio-link").classList.add("active");
+});
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function() {
     load_splash();
+    document.getElementById("about-link").classList.add("active");
 });
 
 
