14f7ad2dedbcc5697f026a3e85127cf89f5af50b
Christopher Hosken <hoskenchristopher@gmail.com>
2022-10-04 19:57:34 +0700

    specific skills automation

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,53 @@
 
-$(document).ready(function() {
-    load_splash();
-    document.getElementById("about-link").classList.add("active");
-});
+
+class SkillItem {
+    constructor(name, image, link, skill) {
+        this.name = name;
+        this.image = image;
+        this.link = link;
+        this.skill = skill;
+    }
+}
+
+let skill_list = [
+    new SkillItem("Blender", "../images/software/blender.png", "https://www.blender.org/", 5),
+    new SkillItem("Houdini", "../images/software/houdini.png", "https://www.sidefx.com/", 3),
+    new SkillItem("Unreal Engine 5", "../images/software/unreal.png", "https://www.unrealengine.com/en-UK/", 3),
+    new SkillItem("Nuke", "../images/software/nuke.png", "https://www.foundry.com/products/nuke-family/nuke", 3),
+    new SkillItem("DaVinci Resolve", "../images/software/resolve.png", "https://www.blackmagicdesign.com/products/davinciresolve", 4),
+    new SkillItem("Krita", "../images/software/krita.png", "https://krita.org/en/", 3),
+    new SkillItem("Python", "../images/software/python.png", "https://www.python.org/", 4),
+    new SkillItem("Java", "../images/software/java.png", "https://www.oracle.com/java/", 4),
+    new SkillItem("C++", "../images/software/cpp.png", "https://learn.microsoft.com/en-us/cpp/?view=msvc-170", 3),
+    new SkillItem("HTML, CSS, JS", "../images/software/web.png", "https://www.w3schools.com/html/default.asp", 4),
+];
+
+
+function load_skills() {
+    html = "";
+
+    for (let sdx = 0; sdx < skill_list.length; sdx++) {
+        var skill = skill_list[sdx];
+
+        html += '<a href="' + skill.link + '" target="_blank" class="specific-skill-item">';
+        html += '<img src="' + skill.image + '" alt="' + skill.name + '">';
+        html += '<div class="info">'
+        html += '<p>' + skill.name + '</p>';
+        html += '<div>';
+
+        for (cdx = 1; cdx <= 5; cdx++) {
+            if (cdx <= skill.skill) {
+                html += '<span class="fa fa-star checked"></span>';
+            } else {
+                html += '<span class="fa fa-star"></span>';
+            }
+        }
+
+        html += '</div></div></a>';
+    }
+
+    document.getElementById("specific-skills-container").innerHTML = html;
+}
 
 
 function load_splash() {
@@ -21,4 +66,13 @@ function load_splash() {
         }, 3000);
     }
 }
+
+
+
+
+$(document).ready(function() {
+    load_skills();
+    load_splash();
+});
+
 
