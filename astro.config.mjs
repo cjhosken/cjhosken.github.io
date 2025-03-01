2e67c3399bd3c9af271b9dfc0a584fa0d14af767
cjhosken <hoskenchristopher@gmail.com>
2025-03-01 12:45:34 +0000

    rebuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
+import sitemap from 'astro-sitemap';
 
 // https://astro.build/config
 export default defineConfig({
