8162e9b0470f028d0928c361f77eac65533b0d9f
cjhosken <hoskenchristopher@gmail.com>
2025-04-24 00:03:24 +0100

    base rewrite (SEO)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-import mdx from "@astrojs/mdx"
-import sitemap from 'astro-sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cjhosken.github.io",
-    integrations: [icon(), mdx(), sitemap()],
+    site: 'https://cjhosken.github.io',
+    integrations: [icon()],
 });
