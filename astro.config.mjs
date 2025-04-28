d2ab570bf20f0f25ea98ed304134130a6b88ea17
cjhosken <hoskenchristopher@gmail.com>
2025-04-28 15:32:15 +0100

    updated contact page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://cjhosken.github.io',
-    integrations: [icon()],
-    output: 'server'
+    integrations: [icon(), sitemap()],
 });
