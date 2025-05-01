8cc742780424124bd803bc4d63ac2a9d6d88c353
cjhosken <hoskenchristopher@gmail.com>
2025-05-01 13:06:00 +0100

    added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cjhosken.github.io',
-    integrations: [icon(), sitemap()],
-    output: 'server', // Important!
+  site: 'https://cjhosken.github.io',
+  integrations: [icon(), sitemap()],
+
+  // Important!
+  output: 'server',
+
+  adapter: netlify()
 });
