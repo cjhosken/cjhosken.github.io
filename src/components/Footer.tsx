7924f60b2894ea73888f08780e90a4ca76d2d569
Christopher Hosken <christopherho@electrictheatre.tv>
2025-09-17 17:36:32 +0100

    footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,19 +6,28 @@ export default function Footer() {
         <footer>
             <div className="container">
                 <div id="links">
-                    <a href="/index.html">Home</a>
-                    <a href="">Projects</a>
-                    <a href="">Contact</a>
+                    <p className="header"> Links </p>
+                    <div className="list">
+                        <a href="/index.html">Home</a>
+                        <a href="">Projects</a>
+                        <a href="">Contact</a>
+                    </div>
                 </div>
                 <div id="socials">
-                    <a href="https://www.linkedin.com/in/christopher-hosken/" className="icon"><FaLinkedin/></a>
-                    <a href="https://github.com/cjhosken" className="icon"><FaGithub/></a>
-                    <a href="https://www.youtube.com/@cjhosken" className="icon"><FaYoutube/></a>
+                    <p className="header"> Socials </p>
+                    <div className="list">
+                        <a href="https://www.linkedin.com/in/christopher-hosken/" className="icon" target="_blank"><FaLinkedin/></a>
+                        <a href="https://github.com/cjhosken" className="icon" target="_blank"><FaGithub/></a>
+                        <a href="https://www.youtube.com/@cjhosken" className="icon" target="_blank"><FaYoutube/></a>
+                    </div>
                 </div>
                 <div id="contact">
-                    <a href="mailto:hoskenchristopher@gmail.com">hoskenchristopher@gmail.com</a>
-                    <a href="tel:+447393979912">(+44)7393979912</a>
-                    <a href="" target="_blank" download="ChristopherHosken_CurriculumVitae_2025.pdf">Download Curriculum Vitae (CV)</a>
+                    <p className="header"> Contact </p>
+                    <div className="list">
+                        <a href="mailto:hoskenchristopher@gmail.com">hoskenchristopher@gmail.com</a>
+                        <a href="tel:+447393979912">(+44)7393979912</a>
+                        <a href="" target="_blank" download="ChristopherHosken_CurriculumVitae_2025.pdf">Download Curriculum Vitae (CV)</a>
+                     </div>
                 </div>
             </div>
             <p id="copyright">&copy; {new Date().getFullYear()} Christopher Hosken </p>
